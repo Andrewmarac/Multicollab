ebfd8e57d90fb6a4a096aa81c4d0d9773ba23790
Andrewmarac <marasigan.joshua.andrew@iisgalvanimi.edu.it>
2021-05-18 16:28:20 +0000

    NON RICORDO COSA HO FATTO

--- MultiClient/src/app/app-routing.module.ts
+++ MultiClient/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AboutComponent } from './about/about.component';
+import { InfluencerComponent } from './influencer/influencer.component';
+const routes: Routes = [
+    { path:'about',component:AboutComponent},
+    { path:'influencer',component:InfluencerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- MultiClient/src/app/app.module.ts
+++ MultiClient/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InfluencerComponent } from './influencer/influencer.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InfluencerComponent
+    InfluencerComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
